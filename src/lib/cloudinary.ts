c406a6a89a79b77ac203515ae789cddbcafc6d99
akaexgot <joaquinrivtir@gmail.com>
2026-03-16 22:06:45 +0100

    feat: enable video upload and display in gallery

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -10,7 +10,7 @@ cloudinary.config({
 export const uploadToCloudinary = async (buffer: Uint8Array, folder: string) => {
     return new Promise((resolve, reject) => {
         cloudinary.uploader.upload_stream(
-            { folder },
+            { folder, resource_type: 'auto' },
             (error, result) => {
                 if (error) return reject(error);
                 resolve(result);
